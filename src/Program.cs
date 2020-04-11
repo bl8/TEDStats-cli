2554dcde1282a337aa4442bb73e896c9073f8218
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2020-04-11 17:30:08 +0200

    cli: Add "query", "from" and "to" parameters

--- src/Program.cs
+++ src/Program.cs
@@ -4,19 +4,51 @@
 //
 
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 using TEDStats.Api;
+using TEDStats.Model;
 
 namespace TEDStats
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand {
+                new Option<string>(new string[] {"--query", "-q"}) {
+                    Required = true,
+                    Description = "Search query"
+                },
+                new Option<string>("--from") {
+                    Required = true,
+                    Description = "Start date, formatted as YYYY-MM-DD"
+                },
+                new Option<string>("--to", getDefaultValue: () => DateTime.UtcNow.ToString("yyyy-MM-dd")) {
+                    Description = "End date, formatted as YYYY-MM-DD. Defaults to the current date"
+                }
+            };
+            
+            rootCommand.TreatUnmatchedTokensAsErrors = true;
+            rootCommand.Handler = CommandHandler.Create<string, string, string>(GetStats);
+            rootCommand.InvokeAsync(args).Wait();
+        }
+
+        public static async Task GetStats(string query, string from, string to)
         {
             var api = new SearchApi();
-            var res = await api.Search("ND=[170115-2020]");
+
+            var full_query = String.Format("({0}) AND PD=[{1} <> {2}]", query, from.Replace("-", ""), to.Replace("-", ""));
+            var req = new SearchRequest(full_query) {
+                Scope = 2
+            };
+            System.Console.WriteLine(req.ToJson());
+            
+            var res = await api.Search(req);
             System.Console.WriteLine("Results count: " + res.Total);
-            System.Console.WriteLine(res.Results[0].ToString());
+            //System.Console.WriteLine(res.Results[0].ToString());
         }
     }
 }
